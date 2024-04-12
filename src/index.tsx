75d2b863d3f7215074a150ea69e904262fb23dd1
SedoyDi <95245699+SedoyDi@users.noreply.github.com>
2024-04-12 11:26:36 +0300

    feet: app v 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import AppV2 from './AppV2';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AppV2 />
+    {/* <App /> */}
   </React.StrictMode>
 );
 ;
